0a4f6ce6a8c597927342a919c5f83a3eb799ff83
Kindeep Singh Kargil <kindeepsingh@gmail.com>
2020-09-26 13:13:19 -0400

    state start, change project structure

--- src/App.js
+++ src/App.js
@@ -1,42 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import Project from "./components/Project";
 
+const mockProjects = {
+  p1: {
+    id: "p1",
+    title: "My cool project 1",
+    description: "I did a lot of nice things with this project",
+    links: [
+      {
+        name: "GitHub",
+        url: "https://www.github.com/brockcsc/brockcsc.github.io",
+      },
+    ],
+  },
+  p2: {
+    id: "p2",
+    title: "My cool project 2",
+    description: "I did a lot of nice things with this project",
+    links: [
+      {
+        name: "GitHub",
+        url: "https://www.github.com/brockcsc/brockcsc.github.io",
+      },
+    ],
+  },
+  p3: {
+    id: "p3",
+    title: "My cool project 3",
+    description: "I did a lot of nice things with this project",
+    links: [
+      {
+        name: "GitHub",
+        url: "https://www.github.com/brockcsc/brockcsc.github.io",
+      },
+    ],
+  },
+};
+
 function App() {
-  const projects = [
-    {
-      title: "My cool project 1",
-      description: "I did a lot of nice things with this project",
-      links: [
-        {
-          name: "GitHub",
-          url: "https://www.github.com/brockcsc/brockcsc.github.io",
-        },
-      ],
-    },
-    {
-      title: "My cool project 2",
-      description: "I did a lot of nice things with this project",
-      links: [
-        {
-          name: "GitHub",
-          url: "https://www.github.com/brockcsc/brockcsc.github.io",
-        },
-      ],
-    },
-    {
-      title: "My cool project 3",
-      description: "I did a lot of nice things with this project",
-      links: [
-        {
-          name: "GitHub",
-          url: "https://www.github.com/brockcsc/brockcsc.github.io",
-        },
-      ],
-    },
-  ];
+  const [projects, setProjects] = useState(mockProjects);
+
+  const [num, setNum] = useState(0);
+
   return (
     <div>
-      {projects.map((project) => (
+      {num}
+      <button
+        onClick={() => {
+          setNum((num) => num + 1);
+        }}
+      >
+        Do stuff
+      </button>
+      {Object.values(projects).map((project) => (
         <Project project={project}></Project>
       ))}
     </div>
